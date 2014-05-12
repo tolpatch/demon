abb3ef9067af590b1ad1592390c29c8c708b5a23
nacim <tolpatch@gmail.com>
2014-05-12 16:36:51 +0200

    Inscription utilisateur finie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,21 @@ class UsersController < ApplicationController
 def create
     User.create(user_params)
   end	
-	
+  
+  def create
+    @user = User.new(params[:user].permit(:nom, :email,:password,:password_confirmation))
+    if @user.save
+	 flash[:success] = "Bienvenue dans l'Application Exemple!"
+     redirect_to @user
+    else
+      @titre = "Inscription"
+      render 'new'
+    end
+  end	
 
   
   def new
+    @user = User.new
   @titre= "Inscription"
   end
   
